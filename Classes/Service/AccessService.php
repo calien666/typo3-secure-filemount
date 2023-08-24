7c945979a414da5b68e08e791c609524a0d3c32f
mhofmann <m.hofmann@web-vision.de>
2023-08-24 17:18:37 +0200

    [TASK] Refactor for access per directory
    With this commit the access is checked by folder access, but not even by
    File Storage.

--- Classes/Service/AccessService.php
+++ Classes/Service/AccessService.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 
 namespace Calien\SecureFilemount\Service;
 
-use Doctrine\DBAL\Driver\Exception;
+use Calien\SecureFilemount\Domain\Model\Folder;
+use Calien\SecureFilemount\Domain\Repository\FolderRepository;
 use TYPO3\CMS\Core\Context\Context;
 use TYPO3\CMS\Core\Context\Exception\AspectNotFoundException;
 use TYPO3\CMS\Core\Context\UserAspect;
-use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Resource\Exception\InvalidPathException;
 use TYPO3\CMS\Core\Resource\ResourceStorage;
 use TYPO3\CMS\Core\Resource\StorageRepository;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
@@ -32,34 +33,26 @@ class AccessService
     }
 
     /**
-     * @param ResourceStorage $storage
-     * @return bool
      * @throws AspectNotFoundException
-     * @throws Exception
      */
-    public static function checkStorageAccess(ResourceStorage $storage): bool
+    public static function checkResourceAccess(ResourceStorage $storage, string $identifier): bool
     {
+        if (
+            GeneralUtility::makeInstance(Context::class)
+                ->getPropertyFromAspect('backend.user', 'isLoggedIn')
+        ) {
+            return true;
+        }
         /** @var UserAspect $feUser */
         $feUser = GeneralUtility::makeInstance(Context::class)->getAspect('frontend.user');
         if (!$feUser->isLoggedIn()) {
             return false;
         }
 
+        $datasetGroupArray = $feUser->getGroupIds();
+        $folderAccess = self::findDirectoryAccess($storage, $identifier);
         $storageRecord = $storage->getStorageRecord();
-
-        $groupArray = GeneralUtility::intExplode(',', $storageRecord['fe_group']);
-
-        $datasetGroupArray = [];
-        $db = GeneralUtility::makeInstance(ConnectionPool::class)->getConnectionForTable('fe_users');
-        $fetch = $db
-            ->select(
-                ['*'],
-                'fe_users',
-                ['uid' => $feUser->get('id')]
-            )->fetchAssociative();
-        if ($fetch) {
-            $datasetGroupArray = GeneralUtility::trimExplode(',', $fetch['usergroup']);
-        }
+        $groupArray = $folderAccess ? $folderAccess->getFeGroup() : GeneralUtility::intExplode(',', $storageRecord['fe_group']);
 
         // check if user has group or enabled for every login
         if (
@@ -70,5 +63,21 @@ class AccessService
             return true;
         }
         return false;
+    }
+
+    private static function findDirectoryAccess(ResourceStorage $storage, string $identifier): ?Folder
+    {
+        $currentFolderIdentifier = $storage->getFolderIdentifierFromFileIdentifier($identifier);
+        $currentFolder = $storage->getFolder($currentFolderIdentifier);
+        $accessibleFolder = GeneralUtility::makeInstance(FolderRepository::class)
+            ->findFolderByHash($storage, $currentFolder->getHashedIdentifier());
+        if (!($accessibleFolder instanceof Folder)) {
+            try {
+                $accessibleFolder = self::findDirectoryAccess($storage, $currentFolderIdentifier);
+            } catch (InvalidPathException $_) {
+                return null;
+            }
+        }
+        return $accessibleFolder;
     }
 }
