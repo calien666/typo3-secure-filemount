5e62b39e3fb94c3e6f674660597f8bfe9dbdf517
mhofmann <m.hofmann@web-vision.de>
2023-08-25 16:46:43 +0200

    [TASK] Prepare release workflow

--- ext_localconf.php
+++ ext_localconf.php
@@ -7,6 +7,6 @@
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['Backend\Template\Components\ButtonBar']['getButtonsHook'][1679002016878]
         = \Calien\SecureFilemount\Hooks\ButtonBarHook::class . '->renderButtons';
 
-    $GLOBALS ['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] =
-        \Calien\SecureFilemount\Hooks\DataMapperHook::class;
+    $GLOBALS ['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][]
+        = \Calien\SecureFilemount\Hooks\DataMapperHook::class;
 })();
