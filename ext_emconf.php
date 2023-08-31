024233adb2e31380df9f3f0bb16428b562d16013
mhofmann <m.hofmann@web-vision.de>
2023-08-31 12:02:28 +0200

    [TASK] Fix ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,15 +3,24 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Secure Filemount',
     'description' => 'Allows a secure (non public) filemount for accessing only through logged in fe_user',
-    'category' => 'templates',
+    'category' => 'services',
     'autoload' => [
         'psr-4' => [
             'Calien\\SecureFilemount\\' => 'Classes',
         ],
     ],
+    'constraints' => [
+        'depends' => [
+            'typo3' => '11.5.0-11.5.99',
+            'php' => '7.4.0-8.1.99',
+        ],
+        'conflicts' => [
+            'fal_securedownload' => '',
+            'secure_downloads' => '',
+            'fal_protect' => '',
+        ],
+    ],
     'state' => 'stable',
-    'uploadfolder' => 0,
-    'createDirs' => '',
     'author' => 'Markus Hofmann',
     'author_email' => 'typo3@calien.de',
     'author_company' => '',
