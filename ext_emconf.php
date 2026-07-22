8b3fd46d782a709a82f2904a530bb011b0d7d7e8
Markus Hofmann <m.hofmann@web-vision.de>
2026-07-22 12:04:56 +0200

    [TASK] Prepare release 2.0.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -27,5 +27,5 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Markus Hofmann',
     'author_email' => 'typo3@calien.de',
     'author_company' => '',
-    'version' => '2.0.0',
+    'version' => '2.0.1',
 ];
